eda74e509979770de7bc8a5b63fd08a2b37cd2c0
abdullah-aljoulani <abdullahaljoulani@gmail.com>
2024-11-01 13:18:36 +0000

    first two testes

---
+++ src/myTestCases.java
@@ -0,0 +1,81 @@
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class myTestCases {
+
+	WebDriver driver = new ChromeDriver();
+
+	String website = "https://codenboxautomationlab.com/practice/";
+
+	Random rand = new Random();
+
+	@BeforeTest
+	public void setUp() {
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		driver.manage().window().maximize();
+
+		driver.get(website);
+
+	}
+
+	@Test(priority = 1, enabled = false)
+	public void RadioButton() {
+
+		WebElement radioDev = driver.findElement(By.id("radio-btn-example"));
+
+		radioDev.findElements(By.tagName("input"));
+
+		int randomRadio = rand.nextInt(radioDev.findElements(By.tagName("input")).size());
+
+		WebElement radioButton = radioDev.findElements(By.tagName("input")).get(randomRadio);
+
+		radioButton.click();
+
+		boolean Acutal = radioButton.isSelected();
+		boolean Expected = true;
+
+		Assert.assertEquals(Acutal, Expected);
+
+	}
+
+	@Test(priority = 2, enabled = true)
+	public void DynamicDropdown() throws InterruptedException {
+
+		String[] twoChar = { "az", "fa", "ua", "ma" };
+
+		int randomChar = rand.nextInt(twoChar.length);
+
+		String randInputData = twoChar[randomChar];
+
+		WebElement textFeild = driver.findElement(By.id("autocomplete"));
+
+		textFeild.sendKeys(randInputData);
+
+		Thread.sleep(1000);
+
+		textFeild.sendKeys(Keys.chord(Keys.ARROW_DOWN, Keys.ENTER));
+
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+
+		String DataInsideMyInput = (String) js.executeScript("return arguments[0].value", textFeild);
+
+		boolean Acuail = DataInsideMyInput.contains(randInputData.toLowerCase());
+
+		Assert.assertEquals(Acuail, true);
+
+	}
+
+}
